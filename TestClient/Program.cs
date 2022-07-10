Headpenguin/SmoOnlineServer

commit f29c4e671ecab472c9a228f0af0c87b8859c0174

Added a voice chat packet broadcaster

TestClient/Program.cs
@@ -62,6 +62,22 @@ async Task S(string n, Guid otherId, Guid ownId) {
         connect.Serialize(connectOwner.Memory.Span[Constants.HeaderSize..(Constants.HeaderSize + connect.Size)]);
         await stream.WriteAsync(connectOwner.Memory[..(Constants.HeaderSize + connect.Size)]);
         connectOwner.Dispose();
+		PlayerPacket setPosition = new PlayerPacket {
+			Position = new Vector3(0, 0, 0),
+			Rotation = new Quaternion(1, 0, 0, 0),
+			Act = 0,
+			SubAct = 0
+		};
+		PacketHeader positionHeader = new PacketHeader {
+			Type = PacketType.Player,
+			Id = ownId,
+			PacketSize = setPosition.Size
+		};
+		IMemoryOwner<byte> positionBuf = MemoryPool<byte>.Shared.Rent(Constants.HeaderSize + setPosition.Size);
+		positionHeader.Serialize(positionBuf.Memory.Span);
+		setPosition.Serialize(positionBuf.Memory.Span[Constants.HeaderSize..]);
+		await stream.WriteAsync(positionBuf.Memory[..(Constants.HeaderSize + setPosition.Size)]);
+		positionBuf.Dispose();
     }
     
     while (true) {

Server/Program.cs
@@ -126,7 +126,9 @@ server.PacketHandler = (c, p) => {
         case PlayerPacket playerPacket when Settings.Instance.Flip.Enabled
                                             && Settings.Instance.Flip.Pov is FlipOptions.Both or FlipOptions.Others
                                             && Settings.Instance.Flip.Players.Contains(c.Id): {
-            playerPacket.Position += Vector3.UnitY * MarioSize((bool) c.Metadata["2d"]);
+			c.Metadata["position"] = playerPacket.Position;
+            
+			playerPacket.Position += Vector3.UnitY * MarioSize((bool) c.Metadata["2d"]);
             playerPacket.Rotation *= Quaternion.CreateFromRotationMatrix(Matrix4x4.CreateRotationX(MathF.PI))
                                      * Quaternion.CreateFromRotationMatrix(Matrix4x4.CreateRotationY(MathF.PI));
             server.Broadcast(playerPacket, c);
@@ -135,8 +137,10 @@ server.PacketHandler = (c, p) => {
         case PlayerPacket playerPacket when Settings.Instance.Flip.Enabled
                                             && Settings.Instance.Flip.Pov is FlipOptions.Both or FlipOptions.Self
                                             && !Settings.Instance.Flip.Players.Contains(c.Id): {
-            server.BroadcastReplace(playerPacket, c, (from, to, sp) => {
-                if (Settings.Instance.Flip.Players.Contains(to.Id)) {
+			c.Metadata["position"] = playerPacket.Position;
+            
+			server.BroadcastReplace(playerPacket, c, (from, to, sp) => {
+				if (Settings.Instance.Flip.Players.Contains(to.Id)) {
                     sp.Position += Vector3.UnitY * MarioSize((bool) c.Metadata["2d"]);
                     sp.Rotation *= Quaternion.CreateFromRotationMatrix(Matrix4x4.CreateRotationX(MathF.PI))
                                    * Quaternion.CreateFromRotationMatrix(Matrix4x4.CreateRotationY(MathF.PI));
@@ -146,6 +150,9 @@ server.PacketHandler = (c, p) => {
             });
             return false;
         }
+		case PlayerPacket playerPacket:
+			c.Metadata["position"] = playerPacket.Position;
+			break;
     }
 
     return true;

Server/Server.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
+using System.Numerics;
 using Shared;
 using Shared.Packet;
 using Shared.Packet.Packets;
@@ -87,7 +88,6 @@ public class Server {
 				
 				if (r.ReceivedBytes != Constants.HeaderSize)
 					break;
-				Logger.Info($"New chat client with ip {r.RemoteEndPoint}");
 				PacketHeader header = GetHeader(((Memory<byte>) headerBuffer).Span);
 			
 				IMemoryOwner<byte> packetBuf = memoryPool.Rent(header.PacketSize);
@@ -103,6 +103,7 @@ public class Server {
 						string? currIP = null;
 						ChatConnectPacket inPacket = new ChatConnectPacket();
 						inPacket.Deserialize(packetBuf.Memory.Span[..header.PacketSize]);
+						packetBuf.Dispose();
 						
 						if(r.RemoteEndPoint is IPEndPoint ep) {
 							currEP = ep;
@@ -111,7 +112,7 @@ public class Server {
 
 						lock(Clients) {
 							//Check if there is a game client with a matching name, and if so, whether it is not already bound to a chat client
-							if(Clients.Find(c => c.Name == inPacket.Name) is Client gameClient && ((gameClient.ChatEP is null) || (gameClient.ChatEP.Address.ToString() == currIP))) {
+							if(Clients.Find(c => c.Name == inPacket.Name && c.Connected) is Client gameClient && ((gameClient.ChatEP is null) || (gameClient.ChatEP.Address.ToString() == currIP))) {
 								gameClient.ChatEP = currEP;
 
 								ChatInitPacket outPacket = new ChatInitPacket(Settings.Instance.ProximityChat.SilenceRadius, Settings.Instance.ProximityChat.PeakRadius);
@@ -121,8 +122,6 @@ public class Server {
 									Type = PacketType.ChatInit,
 									PacketSize = outPacket.Size,
 								};
-
-								Logger.Warn($"{responseHeader.Id}");
 
 								IMemoryOwner<byte> memory = memoryPool.Rent(Constants.HeaderSize + outPacket.Size);
 
@@ -139,6 +138,23 @@ public class Server {
 						break;
 					
 					case PacketType.ChatVoice:
+						lock(Clients) {
+							if(Clients.Find(c => c.Id == header.Id && c.Connected && c.Metadata.ContainsKey("position")) is Client gameClient) {
+								Vector3 pos = (Vector3) gameClient.Metadata["position"];
+								List<Client> to = Clients.FindAll(c => c.Connected && c.Metadata.ContainsKey("position") && Vector3.Distance((Vector3)c.Metadata["position"]!, pos) < Settings.Instance.ProximityChat.SilenceRadius && c.ChatEP != null);
+								short size = (short) (header.PacketSize + Constants.HeaderSize);
+								IMemoryOwner<byte> mem = memoryPool.Rent(to.Count * size);
+								for(int i = 0; i < to.Count; i++) {
+									(new Memory<byte>(headerBuffer)).CopyTo(mem.Memory[(Index)(i*size)..]);
+									Logger.Warn($"{i}");
+									packetBuf.Memory[..header.PacketSize].CopyTo(mem.Memory[(Index)(i*size+Constants.HeaderSize)..]);
+									ChatVoicePacket.SetDistance(mem.Memory.Span[(Index)(i*size+Constants.HeaderSize)..], Vector3.Distance((Vector3)to[i].Metadata["position"]!, pos));
+								}
+								List<EndPoint> clientEPs = to.Select(c => (EndPoint)c.ChatEP!).ToList();
+								BroadcastUdp(serverSocket, mem, clientEPs, size, token);
+								packetBuf.Dispose();
+							}
+						}
 						break;
 					
 					default:
@@ -168,20 +184,21 @@ public class Server {
 			}
     }
 
-	private async Task<bool> SendToAsyncAndDispose(Socket socket, IMemoryOwner<byte> buf, EndPoint ep, CancellationToken? ct) {
-		int sent = await(ct == null ? socket.SendToAsync(buf.Memory[..Constants.HeaderSize], SocketFlags.None, ep)
-			: socket.SendToAsync(buf.Memory[..Constants.HeaderSize], SocketFlags.None, ep, ct.Value));
-		if(sent < Constants.HeaderSize) {
-			buf.Dispose();
-			return false;
-		}
-		sent = await(ct == null ? socket.SendToAsync(buf.Memory[Constants.HeaderSize..], SocketFlags.None, ep)
-			: socket.SendToAsync(buf.Memory[Constants.HeaderSize..], SocketFlags.None, ep, ct.Value));
-		Logger.Warn($"{BitConverter.ToString(buf.Memory.ToArray())}");
+	private async Task SendToAsync(Socket socket, Memory<byte> buf, EndPoint ep, CancellationToken? ct) {
+		int sent = await(ct == null ? socket.SendToAsync(buf[..Constants.HeaderSize], SocketFlags.None, ep)
+			: socket.SendToAsync(buf[..Constants.HeaderSize], SocketFlags.None, ep, ct.Value));
+		sent = await(ct == null ? socket.SendToAsync(buf[Constants.HeaderSize..], SocketFlags.None, ep)
+			: socket.SendToAsync(buf[Constants.HeaderSize..], SocketFlags.None, ep, ct.Value));
+	}
+
+	private async Task SendToAsyncAndDispose(Socket socket, IMemoryOwner<byte> buf, EndPoint ep, CancellationToken? ct) {
+		SendToAsync(socket, buf.Memory, ep, ct);
 		buf.Dispose();
-		if(sent < buf.Memory.Length - Constants.HeaderSize) 
-			return false;
-		return true;
+	}
+
+	private async Task BroadcastUdp(Socket socket, IMemoryOwner<byte> packets, List<EndPoint> clients, short size, CancellationToken? ct) {
+		await Task.WhenAll(clients.Select( (c, i) => SendToAsync(socket, packets.Memory[(Index)(i*size)..], c, ct) ) );
+		packets.Dispose();
 	}
 
     public static void FillPacket<T>(PacketHeader header, T packet, Memory<byte> memory) where T : struct, IPacket {

Shared/Packet/Packets/ChatVoice.cs
@@ -1,0 +1,37 @@
+using System.Runtime.InteropServices;
+
+namespace Shared.Packet.Packets;
+
+[Packet(PacketType.ChatVoice)]
+public struct ChatVoicePacket : IPacket {
+	
+	public float Timestamp;
+	public float Distance;
+	public byte[] Audio;
+
+	public short Size { get; }
+
+	public ChatVoicePacket(float timestamp, float distance) {
+		Timestamp = timestamp;
+		Distance = distance;
+		Audio = Array.Empty<byte>();
+		Size = 8;
+	}
+
+	public void Serialize(Span<byte> data) {
+		MemoryMarshal.Write(data, ref Timestamp);
+		MemoryMarshal.Write(data[4..], ref Distance);
+		Audio.CopyTo(data[8..]);
+	}
+
+	public void Deserialize(ReadOnlySpan<byte> data) {
+		Timestamp = MemoryMarshal.Read<float>(data);
+		Distance = MemoryMarshal.Read<float>(data[4..]);
+		Audio = data[8..].ToArray();
+	}
+	// Necessary because serialization + deserialization is expensive
+	public static void SetDistance(Span<byte> data, float distance) {
+		MemoryMarshal.Write(data[4..], ref distance);
+	}
+}
+

commit 525637a3d1213162dd181deec7be0efc3c6b6d43

Fixed issues with the test client introduced in commit f29c4e671ecab472c9a228f0af0c87b8859c0174

TestClient/Program.cs
@@ -76,6 +76,7 @@ async Task S(string n, Guid otherId, Guid ownId) {
 		IMemoryOwner<byte> positionBuf = MemoryPool<byte>.Shared.Rent(Constants.HeaderSize + setPosition.Size);
 		positionHeader.Serialize(positionBuf.Memory.Span);
 		setPosition.Serialize(positionBuf.Memory.Span[Constants.HeaderSize..]);
+                await Task.Delay(1000);
 		await stream.WriteAsync(positionBuf.Memory[..(Constants.HeaderSize + setPosition.Size)]);
 		positionBuf.Dispose();
     }